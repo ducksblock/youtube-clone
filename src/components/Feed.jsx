37391c73670588a0fc886294045bdfff38a75a22
ducksblock <pranav@Prnv>
2023-05-11 22:57:03 +0530

    first commit

---
+++ src/components/Feed.jsx
@@ -0,0 +1,27 @@
+import { useState, useEffect } from 'react'
+import { Box, Stack, Typography } from '@mui/material'
+import Sidebar from './Sidebar'
+
+const Feed = () => {
+  return (
+    <Stack sx={{ flexDirection: { sx: 'column', md: 'row' } }}>
+      <Box
+        sx={{
+          height: { sx: 'auto', md: '92vh' },
+          px: { sx: 0, md: 2 },
+        }}
+      >
+        <Sidebar />
+        <Typography
+          className="copyright"
+          variant="body2"
+          sx={{ mt: 1.5, color: '#fff' }}
+        >
+          Copyright 2022 Ducksblock
+        </Typography>
+      </Box>
+    </Stack>
+  )
+}
+
+export default Feed
