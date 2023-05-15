051817a9a048ee2a394ea374f71ef82d1859e7e0
ducksblock <code.d2k1k@aleeas.com>
2023-05-15 15:46:00 +0530

    Added Video Cards in Feed

---
+++ src/components/VideoCard.jsx
@@ -0,0 +1,61 @@
+import { Link } from 'react-router-dom'
+import { Typography, Card, CardContent, CardMedia } from '@mui/material'
+import { CheckCircle } from '@mui/icons-material'
+
+import {
+  demoThumbnailUrl,
+  demoVideoUrl,
+  demoVideoTitle,
+  demoChannelUrl,
+  demoChannelTitle,
+} from '../utils/Constants'
+
+const VideoCard = ({
+  video: {
+    id: { videoId },
+    snippet,
+  },
+}) => {
+  return (
+    <Card
+      sx={{
+        borderRadius: 0,
+        width: { md: '360px', xs: '100%' },
+        boxShadow: 'none',
+        backgroundColor: '#101010',
+      }}
+    >
+      <Link to={videoId ? `/video/${videoId}` : defaultVideoUrl}>
+        <CardMedia
+          image={snippet?.thumbnails?.high?.url}
+          alt={snippet?.title}
+          sx={{
+            borderRadius: 3,
+            width: 360,
+            height: 202,
+          }}
+        />
+        <CardContent sx={{ backgroundColor: '#101010', height: 106 }}>
+          <Link to={videoId ? `/video/${videoId}` : defaultVideoUrl}>
+            <Typography variant="subtitle1" fontWeight="500" color="#F1F1F1">
+              {snippet?.title.slice(0, 60) || demoVideoTitle.slice(0, 60)}
+            </Typography>
+          </Link>
+          <Link
+            to={
+              snippet?.channelId
+                ? `/channel/${snippet?.channelId}`
+                : defaultChannelUrl
+            }
+          >
+            <Typography variant="subtitle2" color="#AAA8A4">
+              {snippet?.channelTitle || demoChannelTitle}
+            </Typography>
+          </Link>
+        </CardContent>
+      </Link>
+    </Card>
+  )
+}
+
+export default VideoCard
